e56e2b4a1055d7d5ad1c52cf7acc4d98a508c8cd
Kolya Movchan <kmovchan261@gmail.com>
2024-12-16 22:09:59 +0200

    feat: extend next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  swcMinify: true,
+  env: {
+    CUSTOM_KEY: process.env.CUSTOM_KEY || "default-value",
+  },
+  i18n: {
+    locales: ["en", "fr"],
+    defaultLocale: "en",
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
